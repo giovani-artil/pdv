0cddce68c99e0de73a9e82aaa8b7418f92d9eb51
giovani-artil <93093777+giovani-artil@users.noreply.github.com>
2021-11-07 20:44:58 -0400

    Delete Vendedor.java

--- Vendedor.java
+++
@@ -1,8 +0,0 @@
-package pdv;
-
-public class Vendedor extends Funcionario{
-	
-	public Vendedor(String nome, String senha, String email, String tele, String cpf) {
-		super(nome, senha, email, tele, cpf);
-	}
-}
